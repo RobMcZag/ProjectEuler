50a7766772a90b2196959c00008912464fa8424c
Roberto Zagni <rz70rz70@gmail.com>
2015-01-16 22:40:39 +0100

    Solution for Project Euler Problem 14

--- src/main/java/com/rzagni/projectEuler/P014_CollatzSequence.java
+++ src/main/java/com/rzagni/projectEuler/P014_CollatzSequence.java
@@ -3,12 +3,77 @@ package com.rzagni.projectEuler;
 public class P014_CollatzSequence {
 
 	public P014_CollatzSequence() {
-		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Calculates next Collatz number from this number.
+	 * @param n The number to calculate the next Collatz number.
+	 * @return The next Collatz number from this number.
+	 */
+	public static long getNextCollatzValue(long n) {
+		checkInputIsGreaterThanZero(n);
+		if (isEven(n)) {
+			return n / 2;
+		} else {
+			return n == 1 ? 1 : 3 * n + 1;
+		}
+		
+	}
+
+	private static boolean isEven(long n) {
+		return (n % 2) == 0;
+	}
+
+	/**
+	 * Calculates the length of the Collatz sequence generated from this number.
+	 * @param n The number to calculate the length of the Collatz sequence.
+	 * @return The length of the Collatz sequence generated from this number.
+	 */
+	public static int getCollatzSequenceLenght(int n) {
+		checkInputIsGreaterThanZero(n);
+		int len = 1;
+		long nextVal = n;
+		while (nextVal > 1) {
+			nextVal = getNextCollatzValue(nextVal);
+			len++;
+		}
+		return len;
+	}
+
+	/**
+	 * Checks if the parameter passed is greater than zero.
+	 * @param n The parameter to be checked.
+	 * @throws IllegalArgumentException if the parameter is not valid.
+	 */
+	protected static void checkInputIsGreaterThanZero(long n) throws IllegalArgumentException {
+		if (n < 1) {
+			throw new IllegalArgumentException("Number should be greter than 0");
+		}
+	}
+
+	/**
+	 * Finds the number, lower or equal than the one passed, that generates the longest Collatz sequence.
+	 * @param n The number to start looking for the longest Collatz sequence.
+	 * @return The length that generates the longest Collatz sequence.
+	 */
+	public static int findMaxCollatzSequenceLenght(int n) {
+		checkInputIsGreaterThanZero(n);
+		int maxLength = 0;
+		int maxLengthNumber = 0;		
+		for (int i = 1; i <= n; i++) {
+			int len = getCollatzSequenceLenght(i);
+			if (len > maxLength) {
+				maxLength = len;
+				maxLengthNumber = i;
+				System.out.format("Nex MAX length is %s for N = %s%n", maxLength, maxLengthNumber);
+			}
+		}
+		return maxLengthNumber;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n = findMaxCollatzSequenceLenght(1000000);
+		System.out.format("MAX length is %s for N = %s%n", getCollatzSequenceLenght(n), n);
 	}
-
 }
+
--- src/test/java/com/rzagni/projectEuler/P014_CollatzSequenceTest.java
+++ src/test/java/com/rzagni/projectEuler/P014_CollatzSequenceTest.java
@@ -29,9 +29,59 @@ public class P014_CollatzSequenceTest {
 	public void tearDown() throws Exception {
 	}
 
+	@Test(expected=IllegalArgumentException.class)
+	public void testCollatzOfZero() {
+
+		P014_CollatzSequence.getNextCollatzValue(0);
+		fail("Should throw an IllegalArgumentException!");
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCollatzValues() {
+
+		assertEquals(1 , P014_CollatzSequence.getNextCollatzValue(1));
+		assertEquals(1 , P014_CollatzSequence.getNextCollatzValue(2));
+		assertEquals(10 , P014_CollatzSequence.getNextCollatzValue(3));
+		assertEquals(2 , P014_CollatzSequence.getNextCollatzValue(4));
+		assertEquals(16 , P014_CollatzSequence.getNextCollatzValue(5));
+		assertEquals(3 , P014_CollatzSequence.getNextCollatzValue(6));
+		assertEquals(22 , P014_CollatzSequence.getNextCollatzValue(7));
+		assertEquals(4 , P014_CollatzSequence.getNextCollatzValue(8));
+		
 	}
 
+	@Test(expected=IllegalArgumentException.class)
+	public void testCollatzSequenceLenghtOfZero() {
+
+		P014_CollatzSequence.getCollatzSequenceLenght(0);
+		fail("Should throw an IllegalArgumentException!");
+	}
+	@Test
+	public void testCollatzSequenceLenght() {
+		assertEquals(1 , P014_CollatzSequence.getCollatzSequenceLenght(1));
+		assertEquals(2 , P014_CollatzSequence.getCollatzSequenceLenght(2));
+		assertEquals(8 , P014_CollatzSequence.getCollatzSequenceLenght(3));
+		assertEquals(3 , P014_CollatzSequence.getCollatzSequenceLenght(4));
+		assertEquals(6 , P014_CollatzSequence.getCollatzSequenceLenght(5));
+		assertEquals(9 , P014_CollatzSequence.getCollatzSequenceLenght(6));
+		assertEquals(20 , P014_CollatzSequence.getCollatzSequenceLenght(9));
+		assertEquals(10 , P014_CollatzSequence.getCollatzSequenceLenght(13));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testFindMaxCollatzSequenceLenghtOfZero() {
+
+		P014_CollatzSequence.findMaxCollatzSequenceLenght(0);
+		fail("Should throw an IllegalArgumentException!");
+	}
+	@Test
+	public void testFindMaxCollatzSequenceLenght() {
+		assertEquals(1 , P014_CollatzSequence.findMaxCollatzSequenceLenght(1));
+		assertEquals(2 , P014_CollatzSequence.findMaxCollatzSequenceLenght(2));
+		assertEquals(3 , P014_CollatzSequence.findMaxCollatzSequenceLenght(3));
+		assertEquals(3 , P014_CollatzSequence.findMaxCollatzSequenceLenght(4));
+		assertEquals(6 , P014_CollatzSequence.findMaxCollatzSequenceLenght(6));
+		assertEquals(9 , P014_CollatzSequence.findMaxCollatzSequenceLenght(13));
+		
+	}
+	
 }
