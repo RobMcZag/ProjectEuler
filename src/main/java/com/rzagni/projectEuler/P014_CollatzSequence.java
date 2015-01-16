a967844298be6d0cc767264bd8d8886f7da58a6f
Roberto Zagni <rz70rz70@gmail.com>
2015-01-16 19:41:31 +0100

    Collatz Sequence exercise start

---
+++ src/main/java/com/rzagni/projectEuler/P014_CollatzSequence.java
@@ -0,0 +1,14 @@
+package com.rzagni.projectEuler;
+
+public class P014_CollatzSequence {
+
+	public P014_CollatzSequence() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
